9bf1d9ea4c6d1e96a0d1d18c93ab5c5b66843ad3
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-05-14 15:29:53 +0530

    pk

--- routes/index.js
+++ routes/index.js
@@ -1,180 +1,199 @@
-var express = require('express');
-const passport = require('passport');
-const googleMaps = require('@google/maps');
-const axios = require('axios');
-const twilio = require('twilio');
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
-const userModel = require('./users');
-const Requirement = require('./requirements');
+var express = require("express");
+const passport = require("passport");
+const googleMaps = require("@google/maps");
+const axios = require("axios");
+const twilio = require("twilio");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+const userModel = require("./users");
+const Requirement = require("./requirements");
 var router = express.Router();
-var flash = require('connect-flash');
+var flash = require("connect-flash");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {error: ""});
+router.get("/", function (req, res, next) {
+  res.render("index", { error: "" });
 });
 
-router.get('/customer', async function(req, res, next) {
-//   try {
-//     const currentKaarigarType = req.params.kaarigarType;
-//     const currentDateTime = new Date();
+router.get("/customer", async function (req, res, next) {
+  //   try {
+  //     const currentKaarigarType = req.params.kaarigarType;
+  //     const currentDateTime = new Date();
 
-//     const activeRequirements = await Requirement.find({
-//         kaarigarType: currentKaarigarType,
-//         expiresAt: { $gt: currentDateTime }
-//     }).populate('customerId'); // This populates the user details
+  //     const activeRequirements = await Requirement.find({
+  //         kaarigarType: currentKaarigarType,
+  //         expiresAt: { $gt: currentDateTime }
+  //     }).populate('customerId'); // This populates the user details
 
-//     res.render('customer', { activeRequirements });
-// } catch (err) {
-//     console.error(err);
-//     res.status(500).send('Error retrieving requirements');
-// }
+  //     res.render('customer', { activeRequirements });
+  // } catch (err) {
+  //     console.error(err);
+  //     res.status(500).send('Error retrieving requirements');
+  // }
 
-const successMessage = req.flash('success')[0];
+  const successMessage = req.flash("success")[0];
 
   const users = await userModel.find();
-  res.render('customer', {users, successMessage});
+  res.render("customer", { users, successMessage });
 });
 
-router.post('/post-requirement', async (req, res) => {
+router.post("/post-requirement", async (req, res) => {
   try {
-      const { kaarigarType, description } = req.body;
-      const createdAt = new Date();
-      const expiresAt = new Date(createdAt.getTime() + (48 * 60 * 60 * 1000)); // 48 hours from now
+    const { kaarigarType, description } = req.body;
+    const createdAt = new Date();
+    const expiresAt = new Date(createdAt.getTime() + 48 * 60 * 60 * 1000); // 48 hours from now
 
-      const newRequirement = new Requirement({
-          User: req.user._id, // Assuming you have the user's ID from the session
-          kaarigarType,
-          description,
-          createdAt,
-          expiresAt
-      });
+    const newRequirement = new Requirement({
+      User: req.user._id, // Assuming you have the user's ID from the session
+      kaarigarType,
+      description,
+      createdAt,
+      expiresAt,
+    });
 
-      await newRequirement.save();
-      req.flash('success', 'Requirements posted successfully.');
-      res.redirect('/customer'); // Redirect after saving
+    await newRequirement.save();
+    req.flash("success", "Requirements posted successfully.");
+    res.redirect("/customer"); // Redirect after saving
   } catch (err) {
-      console.error(err);
-      res.status(500).send('Error posting requirement');
+    console.error(err);
+    res.status(500).send("Error posting requirement");
   }
 });
 
-router.get('/checking', function(req, res, next) {
-  res.render('checking');
+router.get("/checking", function (req, res, next) {
+  res.render("checking");
 });
 
-router.get('/requirement', function(req, res, next) {
-  res.render('requirement');
+router.get("/requirement", function (req, res, next) {
+  res.render("requirement");
 });
 
-router.get('/about', function(req, res){
-  res.render('about');
-})
+router.get("/about", function (req, res) {
+  res.render("about");
+});
 
-router.get('/cv', function(req, res){
-  res.render('check');
-})
+router.get("/cv", function (req, res) {
+  res.render("check");
+});
 
-router.post('/check/sendVerificationCode', async (req, res) => {
+router.post("/check/sendVerificationCode", async (req, res) => {
   try {
     const { mobile } = req.body;
 
-    const sessionCookie = await admin.auth().createSessionCookie(mobile, { expiresIn: 5 * 60 * 1000 });
-    
-    res.cookie('session', sessionCookie, { expires: new Date(Date.now() + 5 * 60 * 1000), httpOnly: true });
-    res.status(200).json({ message: 'Verification code sent successfully.' });
-    res.redirect('/checkverify');
+    const sessionCookie = await admin
+      .auth()
+      .createSessionCookie(mobile, { expiresIn: 5 * 60 * 1000 });
+
+    res.cookie("session", sessionCookie, {
+      expires: new Date(Date.now() + 5 * 60 * 1000),
+      httpOnly: true,
+    });
+    res.status(200).json({ message: "Verification code sent successfully." });
+    res.redirect("/checkverify");
   } catch (error) {
-    console.error('Error sending verification code:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error sending verification code:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.post('/check/verifyCode', async (req, res) => {
+router.post("/check/verifyCode", async (req, res) => {
   try {
     const { code } = req.body;
 
-    const sessionCookie = req.cookies.session || '';
+    const sessionCookie = req.cookies.session || "";
     const user = await admin.auth().verifySessionCookie(sessionCookie);
     const userFromDB = await userModel.findOne({ mobile: user.mobile });
 
     if (userFromDB && userFromDB.verificationCode === code) {
       // Code is valid
-      res.status(200).json({ message: 'Code verified successfully.', user });
+      res.status(200).json({ message: "Code verified successfully.", user });
     } else {
       // Code is invalid
-      res.status(400).json({ error: 'Invalid verification code.' });
+      res.status(400).json({ error: "Invalid verification code." });
     }
   } catch (error) {
-    console.error('Error verifying code:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error verifying code:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.get('/kaarigar', async function(req, res, next) {
+router.get("/kaarigar", async function (req, res, next) {
   const requirements = await Requirement.find().populate("User");
   console.log(requirements);
-  res.render('kaarigar', {requirements});
+  res.render("kaarigar", { requirements });
 });
 
-router.get('/details', async function(req, res, next) {
-  const user = await userModel.findOne({username: req.session.passport.user});
-  res.render('details', {user});
+router.get("/details", async function (req, res, next) {
+  const user = await userModel.findOne({ username: req.session.passport.user });
+  res.render("details", { user });
 });
 
-router.get('/profile', isLoggedIn, async function(req, res){
-  
-  const user = await userModel.findOne({username: req.session.passport.user});
+router.get("/profile", isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({ username: req.session.passport.user });
   // const kaarigar = await kaarigarModel.findOne({username: req.session.passport.kaarigar});
   // if(user)
   // const coordinates = await getCoordinates(user.locationName);
   // console.log(coordinates);
-  res.render('profile', {user});
+  res.render("profile", { user });
   // else res.render('profile', {kaarigar});
 });
 
-router.post('/editdetails', async function(req, res){
+router.post("/editdetails", async function (req, res) {
   const user = await userModel.findOneAndUpdate(
-    {username: req.session.passport.user},
-     {mobile: req.body.mobile,
-    name: req.body.name,
-     email: req.body.email,
-locationName: req.body.locationName,
-username: req.body.username},
-      {new: true});
-      await user.save();
-      res.redirect('profile');
-})
+    { username: req.session.passport.user },
+    {
+      mobile: req.body.mobile,
+      name: req.body.name,
+      email: req.body.email,
+      locationName: req.body.locationName,
+      username: req.body.username,
+    },
+    { new: true }
+  );
+  await user.save();
+  res.redirect("profile");
+});
 
-router.post('/save-location', async (req, res) => {
+router.post("/save-location", async (req, res) => {
   const { lat, lng } = req.query;
-  
+
   console.log(lat);
   console.log(lng);
 
   try {
     // Make a request to OpenStreetMap Nominatim API
-    const response = await axios.get('https://nominatim.openstreetmap.org/reverse', {
+    const response = await axios.get(
+      "https://nominatim.openstreetmap.org/reverse",
+      {
         params: {
-            lat: parseFloat(lat),
-            lon: parseFloat(lng),
-            format: 'json',
-            addressdetails: 1,
+          lat: parseFloat(lat),
+          lon: parseFloat(lng),
+          format: "json",
+          addressdetails: 1,
         },
-    });
+      }
+    );
 
     // Extract the address or location name from the API response
     const locationName = response.data.display_name;
     console.log(locationName);
-    const user = await userModel.findOneAndUpdate({username: req.session.passport.user}, {locationName: locationName, lat: lat, long: lng}, {new: true});
+    const user = await userModel.findOneAndUpdate(
+      { username: req.session.passport.user },
+      { locationName: locationName, lat: lat, long: lng },
+      { new: true }
+    );
     await user.save();
     console.log(user);
-    res.render('profile');
-} catch (error) {
-    console.error('Error converting coordinates to location name:', error.message);
-    res.status(500).json({ error: 'Error converting coordinates to location name' });
-}
+    res.render("profile");
+  } catch (error) {
+    console.error(
+      "Error converting coordinates to location name:",
+      error.message
+    );
+    res
+      .status(500)
+      .json({ error: "Error converting coordinates to location name" });
+  }
   // Create a new user document with the location data
   // const newUser = new userModel({
   //     username: 'exampleUsername', // Use the actual username or user ID
@@ -193,86 +212,99 @@ router.post('/save-location', async (req, res) => {
   // }
 });
 
-  // let transporter = nodemailer.createTransport({
-  //   service: 'gmail',
-  //   auth: {
-  //       user: 'utkarshdixit.2k21@gmail.com',
-  //       pass: 'hxzljnowtchasvxu'
-  //   }
-  // });
+// let transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//       user: 'utkarshdixit.2k21@gmail.com',
+//       pass: 'hxzljnowtchasvxu'
+//   }
+// });
 
-  // let otpStorage = {};
+// let otpStorage = {};
 
 // const twilioClient = twilio('ACbdaaa4fea770e8742a52a0764b4cf5e8', 'de41a802edaa78ef7747ba16033e460b');
 
-  router.post('/register', async function(req, res) {
-    const { selectedOption, mobile, username, email, name, locationName, profession } = req.body;
+router.post("/register", async function (req, res) {
+  const {
+    selectedOption,
+    mobile,
+    username,
+    email,
+    name,
+    locationName,
+    profession,
+  } = req.body;
 
-    if (!username || !name || !mobile || !email || !selectedOption || !locationName) {
-      return res.redirect('/');
-    }
-    let newUser;
-    if(profession !== ""){
-        newUser = new userModel({
-        selectedOption: selectedOption,
-        profession: profession,
-        email: email,
-        mobile: mobile,
-        username: username,
-        name: name,
-        locationName: locationName
+  if (
+    !username ||
+    !name ||
+    !mobile ||
+    !email ||
+    !selectedOption ||
+    !locationName
+  ) {
+    return res.redirect("/");
+  }
+  let newUser;
+  if (profession !== "") {
+    newUser = new userModel({
+      selectedOption: selectedOption,
+      profession: profession,
+      email: email,
+      mobile: mobile,
+      username: username,
+      name: name,
+      locationName: locationName,
+    });
+  } else {
+    newUser = new userModel({
+      selectedOption: selectedOption,
+      mobile: mobile,
+      email: email,
+      username: username,
+      name: name,
+      locationName: locationName,
+    });
+  }
+
+  userModel
+    .register(newUser, req.body.password)
+    .then(() => {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/profile");
       });
-    }else{
-          newUser = new userModel({
-          selectedOption: selectedOption,
-          mobile: mobile,
-          email: email,
-          username: username,
-          name: name,
-          locationName: locationName
-        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.redirect("/", { error: err });
+    });
+  // Generate OTP
+  // const otp = crypto.randomInt(100000, 999999).toString();
 
-    }
+  // // Store user data and OTP in session
+  // req.session.userData = {
+  //   selectedOption,
+  //   mobile,
+  //   username,
+  //   name,
+  //   locationName,
+  //   otp
+  // };
 
-      userModel.register(newUser, req.body.password)
-        .then(() => {
-          passport.authenticate("local")(req, res, function() {
-            res.redirect("/profile");
-          });
-        })
-        .catch((err) => {
-          console.error(err);
-          res.redirect('/', { error: err });
-        });
-    // Generate OTP
-    // const otp = crypto.randomInt(100000, 999999).toString();
+  // // Send OTP via Twilio
+  // try {
+  //   await twilioClient.messages.create({
+  //     body: `Your OTP is: ${otp}`,
+  //     from: '+916394924092',
+  //     to: `+${mobile}`
+  //   });
 
-    // // Store user data and OTP in session
-    // req.session.userData = {
-    //   selectedOption,
-    //   mobile,
-    //   username,
-    //   name,
-    //   locationName,
-    //   otp
-    // };
-
-
-
-    // // Send OTP via Twilio
-    // try {
-    //   await twilioClient.messages.create({
-    //     body: `Your OTP is: ${otp}`,
-    //     from: '+916394924092',
-    //     to: `+${mobile}`
-    //   });
-
-    //   res.render('verifyOtp', { mobile });
-    // } catch (error) {
-    //   console.error('Error sending OTP via Twilio:', error.message);
-    //   res.status(500).send('Error sending OTP');
-    // }
-  });
+  //   res.render('verifyOtp', { mobile });
+  // } catch (error) {
+  //   console.error('Error sending OTP via Twilio:', error.message);
+  //   res.status(500).send('Error sending OTP');
+  // }
+});
 
 // router.post('/verify-otp', (req, res) => {
 //   const { mobile, otp } = req.body;
@@ -307,8 +339,6 @@ router.post('/save-location', async (req, res) => {
 //   }
 // });
 
-
-
 // router.get('/nearby-users', async (req, res) => {
 //   try {
 //       // Retrieve nearby users (adjust the distance as needed)
@@ -327,30 +357,36 @@ router.post('/save-location', async (req, res) => {
 //   }
 // });
 
-router.get('/login', function(req, res){
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/login',passport.authenticate("local", {
-  successRedirect: "/profile",
-  failureRedirect: "/",
-  failureFlash: true
-}), function(req, res){});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+    failureFlash: true,
+  }),
+  function (req, res) {}
+);
 
 router.use((req, res, next) => {
-  res.locals.errorMessage = req.flash('error'); // 'error' is the key used by passport for flash messages
+  res.locals.errorMessage = req.flash("error"); // 'error' is the key used by passport for flash messages
   next();
 });
 
-router.get("/logout", function(req, res){
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/login');
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
   });
 });
 
-function isLoggedIn(req, res, next){
-  if(req.isAuthenticated()) return next();
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
   res.redirect("/login");
 }
 
